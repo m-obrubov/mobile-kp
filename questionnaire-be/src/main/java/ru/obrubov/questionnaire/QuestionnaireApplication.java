d8e7cae4f692ab6d7fb7adcd415e1bf3e33f4313
Maxim Obrubov` <obrubov.max@mail.ru>
2018-09-27 10:14:11 +0300

    Создан проект бэк-сервиса

---
+++ questionnaire-be/src/main/java/ru/obrubov/questionnaire/QuestionnaireApplication.java
@@ -0,0 +1,12 @@
+package ru.obrubov.questionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionnaireApplication.class, args);
+    }
+}
