656856928d7470f2b6cc97cee41a227e3d304caf
Василиса <vasilek_l@inbox.ru>
2018-10-09 15:55:16 +0300

    RESULT CONTROLLER ++ (add)
    Добавление ответа на тест

--- questionnaire-be/src/main/java/ru/obrubov/questionnaire/controller/ResultController.java
+++ questionnaire-be/src/main/java/ru/obrubov/questionnaire/controller/ResultController.java
@@ -3,8 +3,13 @@ package ru.obrubov.questionnaire.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.obrubov.questionnaire.domain.TestResult;
+import ru.obrubov.questionnaire.exception.test.TestNotFoundException;
+import ru.obrubov.questionnaire.response.ErrorResponse;
 import ru.obrubov.questionnaire.response.Response;
+import ru.obrubov.questionnaire.response.result.ResultDataResponse;
 import ru.obrubov.questionnaire.service.TestResultService;
+
+import javax.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/result")
@@ -18,9 +23,14 @@ public class ResultController {
     }
 
     @PostMapping
-    public Response addResult(@RequestBody TestResult testResult) {
-        //TODO
-        TestResult calculatedResult = testResultService.add(testResult);
+    public ResultDataResponse addResult(@RequestBody @NotNull TestResult testResult) {
+        try {
+            TestResult calculatedResult = testResultService.add(testResult);
+            return ResultDataResponse.create(calculatedResult);
+        } catch (TestNotFoundException e) {
+            ErrorResponse.create(400);//TODO
+            e.printStackTrace();
+        }
         return null;
     }
 
