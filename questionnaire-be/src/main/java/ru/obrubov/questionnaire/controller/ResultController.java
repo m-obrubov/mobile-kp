a8bfe7c19f1b53be7b0f5953a1411538b78ff808
Василиса <vasilek_l@inbox.ru>
2018-10-09 17:08:16 +0300

    RESULT CONTROLLER ++ (get own results)
    получение списка результатов текущего пользователя

--- questionnaire-be/src/main/java/ru/obrubov/questionnaire/controller/ResultController.java
+++ questionnaire-be/src/main/java/ru/obrubov/questionnaire/controller/ResultController.java
@@ -3,10 +3,13 @@ package ru.obrubov.questionnaire.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.obrubov.questionnaire.domain.TestResult;
+import ru.obrubov.questionnaire.domain.User;
 import ru.obrubov.questionnaire.exception.test.TestNotFoundException;
 import ru.obrubov.questionnaire.response.ErrorResponse;
 import ru.obrubov.questionnaire.response.Response;
 import ru.obrubov.questionnaire.response.result.ResultDataResponse;
+import ru.obrubov.questionnaire.response.result.ResultsDataResponse;
+import ru.obrubov.questionnaire.security.AccessResolver;
 import ru.obrubov.questionnaire.service.TestResultService;
 
 import javax.validation.constraints.NotNull;
@@ -16,28 +19,32 @@ import javax.validation.constraints.NotNull;
 public class ResultController {
 
     private final TestResultService testResultService;
+    private final AccessResolver accessResolver;
 
     @Autowired
-    public ResultController(TestResultService testResultService) {
+    public ResultController(TestResultService testResultService,
+                            AccessResolver accessResolver) {
         this.testResultService = testResultService;
+        this.accessResolver = accessResolver;
     }
 
     @PostMapping
     public ResultDataResponse addResult(@RequestBody @NotNull TestResult testResult) {
         try {
-            TestResult calculatedResult = testResultService.add(testResult);
+            User user = accessResolver.getCurrentUser();
+            TestResult calculatedResult = testResultService.add(testResult,user);
             return ResultDataResponse.create(calculatedResult);
         } catch (TestNotFoundException e) {
-            ErrorResponse.create(400);//TODO
+            ErrorResponse.create(1002);
             e.printStackTrace();
         }
         return null;
     }
 
     @GetMapping
-    public Response getOwnResults() {
-        //TODO
-        return null;
+    public ResultsDataResponse getOwnResults() {
+        User user = accessResolver.getCurrentUser();
+        return ResultsDataResponse.create(testResultService.getOwnResults(user.getId()));
     }
 
     @GetMapping("/all")
