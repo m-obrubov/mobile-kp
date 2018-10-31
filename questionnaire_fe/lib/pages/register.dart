8c5ffc4a7ba0c178128b072fdfadf50bc11b2dc5
Maxim Obrubov` <obrubov.max@mail.ru>
2018-10-31 21:11:30 +0300

    Реализован примерный интерфейс главной страницы

--- questionnaire_fe/lib/pages/register.dart
+++ questionnaire_fe/lib/pages/register.dart
@@ -15,6 +15,7 @@ class RegisterPageState extends State<RegisterPage> {
   String _city;
   String _email;
   String _password;
+  var _gender;
 
   @override
   Widget build(BuildContext context) {
@@ -22,64 +23,98 @@ class RegisterPageState extends State<RegisterPage> {
       appBar: AppBar(
         title: Text('Регистрация'),
       ),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(labelText: "Имя"),
-                keyboardType: TextInputType.text,
-                validator: _emptyFieldValidator,
-                onFieldSubmitted: (val) => _name = val,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Фамилия"),
-                keyboardType: TextInputType.text,
-                validator: _emptyFieldValidator,
-                onFieldSubmitted: (val) => _surname = val,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Возраст"),
-                keyboardType: TextInputType.number,
-                validator: _emptyFieldValidator,
-                onFieldSubmitted: (val) => _age = val as int,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Город"),
-                keyboardType: TextInputType.text,
-                validator: _emptyFieldValidator,
-                onFieldSubmitted: (val) => _city = val,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Адрес электронной почты"),
-                keyboardType: TextInputType.emailAddress,
-                validator: _emptyFieldValidator,
-                onFieldSubmitted: (val) => _email = val,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Пароль"),
-                keyboardType: TextInputType.text,
-                validator: _emptyFieldValidator,
-                onFieldSubmitted: (val) => _password = val,
-                obscureText: true,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Повторите пароль"),
-                keyboardType: TextInputType.text,
-                validator: _repeatPasswordValidator,
-                obscureText: true,
-              ),
-              RaisedButton(
-                onPressed: _submit,
-                child: Text(
-                  "Зарегистрироваться",
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Имя"),
+                  keyboardType: TextInputType.text,
+                  validator: _emptyFieldValidator,
+                  onFieldSubmitted: (val) => _name = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Фамилия"),
+                  keyboardType: TextInputType.text,
+                  validator: _emptyFieldValidator,
+                  onFieldSubmitted: (val) => _surname = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Возраст"),
+                  keyboardType: TextInputType.number,
+                  validator: _emptyFieldValidator,
+                  onFieldSubmitted: (val) => _age = val as int,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Город"),
+                  keyboardType: TextInputType.text,
+                  validator: _emptyFieldValidator,
+                  onFieldSubmitted: (val) => _city = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Адрес электронной почты"),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: _emptyFieldValidator,
+                  onFieldSubmitted: (val) => _email = val,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Пароль"),
+                  keyboardType: TextInputType.text,
+                  validator: _emptyFieldValidator,
+                  onFieldSubmitted: (val) => _password = val,
+                  obscureText: true,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Повторите пароль"),
+                  keyboardType: TextInputType.text,
+                  validator: _repeatPasswordValidator,
+                  obscureText: true,
+                ),
+                Text(
+                  "Пол:",
                   style: TextStyle(fontSize: 18.0),
                 ),
-              ),
-            ],
+                Container(
+                  padding: EdgeInsets.only(left: 2.0),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Radio<String>(
+                            value: 'MALE',
+                            groupValue: _gender,
+                            onChanged: _handleGenderChoice,
+                          ),
+                          Text("Мужской"),
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Radio<String>(
+                            value: 'FEMALE',
+                            groupValue: _gender,
+                            onChanged: _handleGenderChoice,
+                          ),
+                          Text("Женский"),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+
+                RaisedButton(
+                  onPressed: _submit,
+                  child: Text(
+                    "Зарегистрироваться",
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       )
@@ -88,6 +123,7 @@ class RegisterPageState extends State<RegisterPage> {
 
   void _submit() {
     if (_formKey.currentState.validate()) {
+      //TODO вызвать API регистрации
       //TODO сбрасывать route
       Navigator
           .of(context)
@@ -112,5 +148,18 @@ class RegisterPageState extends State<RegisterPage> {
       return 'Введите верный пароль';
     }
     return '';
+  }
+
+  void _handleGenderChoice(String value) {
+    setState(() {
+      switch(value) {
+        case 'MALE':
+          _gender = 'MALE';
+          break;
+        case 'FEMALE':
+          _gender = 'FEMALE';
+          break;
+      }
+    });
   }
 }
