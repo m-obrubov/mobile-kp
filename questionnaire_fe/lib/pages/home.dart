9ae3d79244a5be800c62aa7db05a04eee77d90e6
Maxim Obrubov` <obrubov.max@mail.ru>
2018-11-06 16:26:34 +0300

    Рефакторинг. Изменена главная страница: убараны padding`и у прокручиваемого текста

--- questionnaire_fe/lib/pages/home.dart
+++ questionnaire_fe/lib/pages/home.dart
@@ -46,57 +46,58 @@ class Home extends StatelessWidget {
         title: Text('Главная'),
         actions: profileIcon
       ),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset(
-                      'images/logo.png',
-                      alignment: Alignment.center,
-                      width: 100.0,
-                      height: 100.0,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Image.asset(
+                    'images/logo.png',
+                    alignment: Alignment.center,
+                    width: 100.0,
+                    height: 100.0,
+                  ),
+                  Text(
+                    "Описание",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0
                     ),
-                    Text(
-                      "Описание",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0
-                      ),
+                  ),
+                  Text(
+                    "Анкета 'Ориентация' определяет профессиональную направленность "
+                        "личности к определенной сфере деятельности.",
+                    style: TextStyle(
+                        fontSize: 18.0
                     ),
-                    Text(
-                      "Анкета 'Ориентация' определяет профессиональную направленность "
-                          "личности к определенной сфере деятельности.",
-                      style: TextStyle(
-                          fontSize: 18.0
-                      ),
+                  ),
+                  Divider(),
+                  Text(
+                    "Правила",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0
                     ),
-                    Divider(),
-                    Text(
-                      "Правила",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0
-                      ),
+                  ),
+                  Text(
+                    "В первой части (\"Я хочу\") вы можете оценить по 4-х балльной "
+                        "шкале степень своего желания заниматься...",
+                    style: TextStyle(
+                        fontSize: 18.0
                     ),
-                    Text(
-                      "В первой части (\"Я хочу\") вы можете оценить по 4-х бальной"
-                          "шкале степень своего желания заниматься...",
-                      style: TextStyle(
-                          fontSize: 18.0
-                      ),
-                    ),
-                    Divider(),
-                  ],
-                ),
+                  ),
+                  Divider(),
+                ],
               ),
             ),
-            bodyButtons
-          ],
-        )
+          ),
+          Container(
+            padding: EdgeInsets.all(16.0),
+            child: bodyButtons,
+          )
+        ],
       )
     );
   }
