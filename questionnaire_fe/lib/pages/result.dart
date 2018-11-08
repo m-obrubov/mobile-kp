ada775f5943707767986463aed13143a9c27576a
Василиса <vasilek_l@inbox.ru>
2018-11-08 10:34:45 +0300

    Выпадающее описание

--- questionnaire_fe/lib/pages/result.dart
+++ questionnaire_fe/lib/pages/result.dart
@@ -12,10 +12,10 @@ class Result extends StatelessWidget {
 
   Result(){
     List<Profession> professions = new List();
-    professions.add(new Profession("1","Стоит дома и не только","Архитектор"));
-    professions.add(new Profession("1","Стоит дома и не только","Художник"));
-    professions.add(new Profession("1","Стоит дома и не только","Музыкант"));
-    professions.add(new Profession("1","Стоит дома и не только","Хореограф"));
+    professions.add(new Profession("1","Строит дома и не только!","Архитектор"));
+    professions.add(new Profession("1","Картиты его конек!","Художник"));
+    professions.add(new Profession("1","Что я слышу? Это же музыкант!","Музыкант"));
+    professions.add(new Profession("1","Стой! Стой! Стой! Блин ну ты танцор!","Хореограф"));
     String description = "Вы уверенный в себе и амбициозный человек, для вас нет нечего невозможного. Выша жизнь не возможна без творчества!";
     List<Answer> answers1 = new List();
     List<Answer> answers2 = new List();
@@ -38,18 +38,43 @@ class Result extends StatelessWidget {
     Widget widgetResultQuestions;
 
     for (int i = 0; i < _result.professions.length; i++) {
-      String professions = _result.professions[i].value;
+      String _professionsDescription = _result.professions[i].description;
+      String _professionsName = _result.professions[i].value;
       listProfessions.add (
-        ListTile(
-          title:
-            Text(professions,
-                style: TextStyle(
-                fontSize: 18.0
-                )
-            ),
-          onTap: () => moveWithHistory(context, null),
-          trailing: Icon(Icons.keyboard_arrow_right),
-        )
+
+//        ListTile(
+//          title:
+//            Text(professions,
+//                style: TextStyle(
+//                fontSize: 18.0
+//                )
+//            ),
+//         // onTap: () =>  new Text("описание"),
+//          trailing: Icon(Icons.keyboard_arrow_right),
+//        ),
+//            DropdownButton<String>(
+//              items: <String>['А', 'B', 'C', 'D'].map((String professions) {
+//                return new DropdownMenuItem<String>(
+//                  value: professions,
+//                  child: new ListTile(
+//                    title: Text(professions),
+//                  ),
+//                );
+//              }).toList(),
+//              onChanged: (_) {},
+//            ),
+          DropdownButton<String>(
+            items: <String>[_professionsName,_professionsDescription].map((String value) {
+              return new DropdownMenuItem<String>(
+                value: value,
+                child: new Text(value),
+              );
+            }).toList(),
+            value: _professionsName,
+            isExpanded: true,
+            //style: TextStyle( fontSize: 13),
+            onChanged: (_) {},
+          )
       );
     }
 
