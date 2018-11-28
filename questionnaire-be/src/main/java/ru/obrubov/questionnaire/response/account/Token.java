e615aaa9430e008378ea45d46da1485119c33686
Василиса <vasilek_l@inbox.ru>
2018-11-28 15:20:44 +0300

    get and set token

--- questionnaire-be/src/main/java/ru/obrubov/questionnaire/response/account/Token.java
+++ questionnaire-be/src/main/java/ru/obrubov/questionnaire/response/account/Token.java
@@ -2,6 +2,8 @@ package ru.obrubov.questionnaire.response.account;
 
 import ru.obrubov.questionnaire.domain.Role;
 
+import javax.persistence.JoinColumn;
+
 public class Token {
     private String token;
     private long lifetime; //время жизни токена
@@ -13,7 +15,30 @@ public class Token {
         this.role = role;
     }
 
+    @JoinColumn(name = "token")
     public String getToken() {
         return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @JoinColumn(name = "lifetime")
+    public long getLifetime() {
+        return lifetime;
+    }
+
+    public void setLifetime(long lifetime) {
+        this.lifetime = lifetime;
+    }
+
+    @JoinColumn(name = "role")
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 }
