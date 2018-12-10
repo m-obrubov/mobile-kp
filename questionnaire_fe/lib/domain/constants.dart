6656d8a655e730c11b41b83b7c1f437323f873dc
Василиса <vasilek_l@inbox.ru>
2018-12-10 19:56:00 +0300

    Добавление констант

--- questionnaire_fe/lib/domain/constants.dart
+++ questionnaire_fe/lib/domain/constants.dart
@@ -18,6 +18,53 @@ class Gender extends Constant {
   }
 }
 
+class Work extends Constant {
+  static final Work CAN = new Work('Могу', 'CAN');
+  static final Work WANT = new Work('Хочу', 'WANT');
+  static final Work HUMAN = new Work('Человек', 'HUMAN');
+  static final Work TECHNICS = new Work('Техника', 'TECHNICS');
+  static final Work ZNAK = new Work('Знак', 'ZNAK');
+  static final Work ART = new Work('Творчество', 'ART');
+  static final Work NATURE = new Work('Природа', 'NATURE');
+  static final Work EXECUTOR = new Work('Исполнитель', 'EXECUTOR');
+  static final Work CREATOR = new Work('Создатель', 'CREATOR');
+
+  Work(String title, value) : super(title, value);
+
+  factory Work.fromTitle(String title) {
+    switch(title) {
+      case 'CAN':
+        return CAN;
+        break;
+      case 'WANT':
+        return WANT;
+        break;
+      case 'HUMAN':
+        return HUMAN;
+        break;
+      case 'TECHNICS':
+        return TECHNICS;
+        break;
+      case 'ZNAK':
+        return ZNAK;
+        break;
+      case 'ART':
+        return ART;
+        break;
+      case 'NATURE':
+        return NATURE;
+        break;
+      case 'EXECUTOR':
+        return EXECUTOR;
+        break;
+      case 'CREATOR':
+        return CREATOR;
+        break;
+      default:
+        return null;
+    }
+  }
+}
 class Constant {
   final String title;
   var value;
