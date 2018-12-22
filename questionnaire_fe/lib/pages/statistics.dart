d866f809126df6774b2df7b3832ae5ad47c66438
Maxim Obrubov <maxim.obrubov@bsc-ideas.com>
2018-12-22 15:16:35 +0300

    Отображаются графики статистики с легендой

--- questionnaire_fe/lib/pages/statistics.dart
+++ questionnaire_fe/lib/pages/statistics.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:questionnaire_fe/domain/constants.dart';
 import 'package:questionnaire_fe/domain/resultTest.dart';
 import 'package:questionnaire_fe/pages/navigation.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
@@ -13,100 +14,14 @@ class StatisticsPage extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() => new _StatisticsPageState(this.results);
-
 }
 
 class _StatisticsPageState extends State<StatisticsPage> with SingleTickerProviderStateMixin {
 
   TabController _tabController;
-
   final List<ResultTest> _results;
-  List<charts.Series> seriesList;
-  List<charts.Series> seriesList2;
-  List<charts.Series> seriesList3;
-  bool animate;
 
-  /// Create one series with sample hard coded data.
-  static List<charts.Series<LinearSales, int>> _createSampleData() {
-    final data = [
-      new LinearSales(0, 100),
-      new LinearSales(1, 75),
-      new LinearSales(2, 25),
-      new LinearSales(3, 5),
-    ];
-
-    return [
-      new charts.Series<LinearSales, int>(
-        id: 'Sales',
-        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-        domainFn: (LinearSales sales, _) => sales.year,
-        measureFn: (LinearSales sales, _) => sales.sales,
-        data: data,
-        // Set a label accessor to control the text of the arc label.
-        labelAccessorFn: (LinearSales row, _) => '${row.year}: ${row.sales}',
-      )
-    ];
-
-  }
-
-  static List<charts.Series<OrdinalSales, String>> _createSampleData2() {
-    final data = [
-      new OrdinalSales('2014', 5),
-      new OrdinalSales('2015', 25),
-      new OrdinalSales('2016', 100),
-      new OrdinalSales('2017', 75),
-    ];
-
-    return [
-      new charts.Series<OrdinalSales, String>(
-        id: 'Sales',
-        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-        domainFn: (OrdinalSales sales, _) => sales.year,
-        measureFn: (OrdinalSales sales, _) => sales.sales,
-        data: data,
-      )
-    ];
-  }
-
-  static List<charts.Series<TimeSeriesSales, DateTime>> _createSampleData3() {
-    final data = [
-      new TimeSeriesSales(new DateTime(2017, 9, 1), 5),
-      new TimeSeriesSales(new DateTime(2017, 9, 2), 5),
-      new TimeSeriesSales(new DateTime(2017, 9, 3), 25),
-      new TimeSeriesSales(new DateTime(2017, 9, 4), 100),
-      new TimeSeriesSales(new DateTime(2017, 9, 5), 75),
-      new TimeSeriesSales(new DateTime(2017, 9, 6), 88),
-      new TimeSeriesSales(new DateTime(2017, 9, 7), 65),
-      new TimeSeriesSales(new DateTime(2017, 9, 8), 91),
-      new TimeSeriesSales(new DateTime(2017, 9, 9), 100),
-      new TimeSeriesSales(new DateTime(2017, 9, 10), 111),
-      new TimeSeriesSales(new DateTime(2017, 9, 11), 90),
-      new TimeSeriesSales(new DateTime(2017, 9, 12), 50),
-      new TimeSeriesSales(new DateTime(2017, 9, 13), 40),
-      new TimeSeriesSales(new DateTime(2017, 9, 14), 30),
-      new TimeSeriesSales(new DateTime(2017, 9, 15), 40),
-      new TimeSeriesSales(new DateTime(2017, 9, 16), 50),
-      new TimeSeriesSales(new DateTime(2017, 9, 17), 30),
-      new TimeSeriesSales(new DateTime(2017, 9, 18), 35),
-      new TimeSeriesSales(new DateTime(2017, 9, 19), 40),
-      new TimeSeriesSales(new DateTime(2017, 9, 20), 32),
-      new TimeSeriesSales(new DateTime(2017, 9, 21), 31),
-    ];
-
-    return [
-      new charts.Series<TimeSeriesSales, DateTime>(
-        id: 'Sales',
-        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-        domainFn: (TimeSeriesSales sales, _) => sales.time,
-        measureFn: (TimeSeriesSales sales, _) => sales.sales,
-        data: data,
-      )
-    ];
-  }
-
-  _StatisticsPageState(this._results){
-    animate = false;
-  }
+  _StatisticsPageState(this._results);
 
   @override
   void initState() {
@@ -116,12 +31,46 @@ class _StatisticsPageState extends State<StatisticsPage> with SingleTickerProvid
 
   @override
   Widget build(BuildContext context) {
+
+    var seriesCan = [
+      new charts.Series(
+        id: 'Исполнитель',
+        domainFn: (_TestPerType testData, _) => testData.type.title,
+        measureFn: (_TestPerType testData, _) => testData.count,
+        data: _prepareChartData(Work.EXECUTOR, true),
+      ),
+      new charts.Series(
+        id: 'Создатель',
+        domainFn: (_TestPerType testData, _) => testData.type.title,
+        measureFn: (_TestPerType testData, _) => testData.count,
+        data: _prepareChartData(Work.CREATOR, true),
+      ),
+    ];
+
+    var seriesWant = [
+      new charts.Series(
+        id: 'Исполнитель',
+        domainFn: (_TestPerType testData, _) => testData.type.title,
+        measureFn: (_TestPerType testData, _) => testData.count,
+        data: _prepareChartData(Work.EXECUTOR, false),
+      ),
+      new charts.Series(
+        id: 'Создатель',
+        domainFn: (_TestPerType testData, _) => testData.type.title,
+        measureFn: (_TestPerType testData, _) => testData.count,
+        data: _prepareChartData(Work.CREATOR, false),
+      ),
+    ];
+
+    var chartCan = _getBarChart(seriesCan);
+    var chartWant = _getBarChart(seriesWant);
+
     return Scaffold(
         appBar: AppBar(
           bottom: TabBar(
             tabs: [
-              Tab(icon: Icon(Icons.youtube_searched_for),text: "Результаты поиска",),
-              Tab(icon: Icon(Icons.equalizer),text: "Графики",)
+              Tab(icon: Icon(Icons.youtube_searched_for),text: "Результаты поиска"),
+              Tab(icon: Icon(Icons.equalizer),text: "Графики")
             ],
             controller: _tabController,
           ),
@@ -130,172 +79,109 @@ class _StatisticsPageState extends State<StatisticsPage> with SingleTickerProvid
         body: TabBarView(
             controller: _tabController,
             children: [
-              _resultList(),
+              ListView.builder(
+                scrollDirection: Axis.vertical,
+                padding: EdgeInsets.all(16.0),
+                itemBuilder: (context, index) {
+                  if (index.isOdd) return Divider(height: 12.0);
+                  final i = index ~/ 2;
+                  return ListTile(
+                    title: Text(
+                        DateFormat('yyyy-MM-dd  kk:mm').format(_results[i].date) + ', ' + _results[i].user.gender.title + ', ' +
+                            _results[i].user.age.toString() + ', ' + _results[i].user.city + '\n'
+                            'Хочу: ' + '"' + _results[i].resultWant.character.title + '-' + _results[i].resultWant.subject.title + '"\n'
+                            'Могу: ' + '"' + _results[i].resultCan.character.title + '-' + _results[i].resultCan.subject.title + '"\n'
+                    ),
+                    onTap: () => moveWithHistory(context, new ResultPage(_results[i])),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  );
+                },
+                itemCount: _results.length * 2,
+
+              ),
               SingleChildScrollView(
                   padding: EdgeInsets.all(16.0),
                   child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
+                        Text('Люди могут:'),
                         Padding(
                             padding: new EdgeInsets.all(32.0),
                             child: new SizedBox(
                                 height: 300.0,
-                                child: charts.PieChart(seriesList, animate: animate)
+                                child: chartCan,
                             )
                         ),
+                        Text('Люди хотят:'),
                         Padding(
                             padding: new EdgeInsets.all(32.0),
                             child: new SizedBox(
                                 height: 300.0,
-                                child: charts.PieChart(seriesList,
-                                    animate: animate,
-                                    defaultRenderer: new charts.ArcRendererConfig(
-                                        arcWidth: 60,
-                                        arcRendererDecorators: [new charts.ArcLabelDecorator()]))
-                            )
-                        ),
-                        Padding(
-                          padding: new EdgeInsets.all(32.0),
-                          child: new SizedBox(
-                              height: 300.0,
-                              child: charts.PieChart(seriesList,
-                                  animate: animate,
-                                  defaultRenderer: new charts.ArcRendererConfig(
-                                      arcWidth: 30, startAngle: 4 / 5 * 3.14, arcLength: 7 / 5 * 3.14))),
-                        ),
-                        Padding(
-                            padding: new EdgeInsets.all(32.0),
-                            child: new SizedBox(
-                                height: 300.0,
-                                child: charts.BarChart(
-                                  seriesList2,
-                                  animate: animate,
-                                  defaultRenderer: new charts.BarRendererConfig(
-                                      cornerStrategy: const charts.ConstCornerStrategy(30)),
-                                )
-                            )
-                        ),
-                        Padding(
-                            padding: new EdgeInsets.all(32.0),
-                            child: new SizedBox(
-                                height: 300.0,
-                                child: charts.TimeSeriesChart(
-                                  seriesList3,
-                                  animate: animate,
-                                  dateTimeFactory: const charts.LocalDateTimeFactory(),
-                                )
-                            )
-                        ),
-                        Padding(
-                            padding: new EdgeInsets.all(32.0),
-                            child: new SizedBox(
-                                height: 300.0,
-                                child: charts.BarChart(
-                                  seriesList2,
-                                  animate: animate,
-                                  barGroupingType: charts.BarGroupingType.grouped,
-                                  vertical: false,
-                                  primaryMeasureAxis: new charts.NumericAxisSpec(
-                                      tickProviderSpec:
-                                      new charts.BasicNumericTickProviderSpec(desiredTickCount: 3)),
-                                  secondaryMeasureAxis: new charts.NumericAxisSpec(
-                                      tickProviderSpec:
-                                      new charts.BasicNumericTickProviderSpec(desiredTickCount: 3)),
-                                )
-                            )
-                        ),
-                        Padding(
-                            padding: new EdgeInsets.all(32.0),
-                            child: new SizedBox(
-                                height: 300.0,
-                                child: charts.BarChart(
-                                  seriesList2,
-                                  animate: animate,
-                                  primaryMeasureAxis: new charts.NumericAxisSpec(
-                                      renderSpec: new charts.SmallTickRendererSpec(
-                                        // Tick and Label styling here.
-                                      )),
-                                )
-                            )
-                        ),
-                        Padding(
-                            padding: new EdgeInsets.all(32.0),
-                            child: new SizedBox(
-                                height: 300.0,
-                                child: charts.LineChart(seriesList,
-                                    defaultRenderer:
-                                    new charts.LineRendererConfig(includeArea: true, stacked: true),
-                                    animate: animate)
-                            )
-                        ),
-                        Padding(
-                            padding: new EdgeInsets.all(32.0),
-                            child: new SizedBox(
-                                height: 300.0,
-                                child: charts.BarChart(seriesList2, animate: animate)
-                            )
-                        ),
-                        Padding(
-                            padding: new EdgeInsets.all(32.0),
-                            child: new SizedBox(
-                                height: 300.0,
-                                child: charts.TimeSeriesChart(
-                                  seriesList3,
-                                  animate: animate,
-                                  defaultRenderer: new charts.BarRendererConfig<DateTime>(),
-                                  domainAxis: new charts.DateTimeAxisSpec(usingBarRenderer: true),
-                                  defaultInteractions: false,
-                                  behaviors: [new charts.SelectNearest(), new charts.DomainHighlighter()],
-                                )
+                                child: chartWant
                             )
                         ),
                       ]
-
                   )
               )
-            ])
+            ]
+        )
+    );
+  }
+  
+  _prepareChartData(Work character, bool can) {
+    Map<Work, int> data = new Map();
+    _results.forEach((res) {
+      if(can) {
+        if(res.resultCan.character == character) {
+          data.update(res.resultCan.subject, (i) => i + 1, ifAbsent: () => 1);
+        }
+      } else {
+        if(res.resultWant.character == character) {
+          data.update(res.resultWant.subject, (i) => i + 1, ifAbsent: () => 1);
+        }
+      }
+    });
+    List<_TestPerType> result = new List();
+    for(var entry in data.entries) {
+      result.add(new _TestPerType(entry.key, entry.value));
+    }
+    return result;
+  }
+
+  Widget _getBarChart(List series) {
+    return charts.BarChart(
+        series,
+        defaultRenderer: new charts.BarRendererConfig(
+          cornerStrategy: const charts.ConstCornerStrategy(30),
+          symbolRenderer: new IconRenderer(Icons.work),
+        ),
+        barGroupingType: charts.BarGroupingType.stacked,
+        behaviors: [new charts.SeriesLegend()],
     );
   }
 
-  Widget _resultList() {
-    List<Widget> listResult = new List();
-    Widget widget;
-
-    for (int i = 0; i < _results.length; i++) {
-      listResult.add(
-        ListTile(
-          title: Text(
-            DateFormat('yyyy-MM-dd  kk:mm').format(_results[i].date) + ', ' + _results[i].user.gender.title + ', ' + _results[i].user.age.toString() + ', ' + _results[i].user.city + '\n'
-                "Хочу: " + '"' + _results[i].resultWant.character.title + '-' + _results[i].resultWant.subject.title + '"\n'
-                "Могу: " + '"' + _results[i].resultCan.character.title + '-' + _results[i].resultCan.subject.title + '"\n'
-          ),
-          onTap: () => moveWithHistory(context, new ResultPage(_results[i])),
-          trailing: Icon(Icons.keyboard_arrow_right),
-        ),
-      );
-    }
-    return new Padding(padding: EdgeInsets.all(16.0), child: Column(children: listResult));
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 }
 
-/// Sample linear data type.
-class LinearSales {
-  final int year;
-  final int sales;
+class _TestPerType {
+  final Work type;
+  final int count;
 
-  LinearSales(this.year, this.sales);
+  _TestPerType(this.type, this.count);
 }
 
-class OrdinalSales {
-  final String year;
-  final int sales;
+class IconRenderer extends charts.CustomSymbolRenderer {
+  final IconData iconData;
 
-  OrdinalSales(this.year, this.sales);
-}
+  IconRenderer(this.iconData);
 
-class TimeSeriesSales {
-  final DateTime time;
-  final int sales;
-
-  TimeSeriesSales(this.time, this.sales);
+  @override
+  Widget build(BuildContext context, {Color color, Size size, bool enabled}) {
+    return new SizedBox.fromSize(
+        size: size, child: new Icon(iconData, color: color, size: 18.0));
+  }
 }
